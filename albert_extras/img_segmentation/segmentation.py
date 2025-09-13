22c35d3aee45e06ac9ba8eb1ad1e3e0047ef616e
Aybert59 <o.messiant@orange.fr>
2025-09-14 00:12:45 +0800

    include start/stop detection engine, independant from display thread

--- albert_extras/img_segmentation/segmentation.py
+++ albert_extras/img_segmentation/segmentation.py
@@ -2,45 +2,77 @@
 # encoding: utf-8
 
 
-
 import sys
 import cv2 as cv
 import numpy as np
 from time import sleep
-
+import threading
 from ultralytics import YOLO
 
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 InputURL = "http://localhost:8080/stream?topic=/camera/rgb/image_raw"  
-model = YOLO("/ultralytics/yolo11n.pt")
+#model = YOLO("/ultralytics/yolo11n.pt")
+model = YOLO("/ultralytics/yolo11n.engine")  # using TensorRT engine for better performance on Jetson Orin NX
 
 # attention versions de numpy, opencv etc.. très spévcifiques... du coup on ne peut rien afficherd'ici. Juste faire un stream 
 
+
 sleep (5)
 cap = None
-#open the video stream and check if it's opened
+results = None
+DetectionThread = None
+stop_event = threading.Event()
 
+def detect_frames():
+    global cap, results
 
-def gen_frames():
-    cap = cv.VideoCapture(InputURL)  # Utilise la webcam
+    #try 5 times to open the video stream
+    for i in range(5):
+        cap = cv.VideoCapture(InputURL)  # Utilise la webcam
+        if cap.isOpened():
+            break
+        sleep(2)    
+
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+        sys.exit(1)
+
     while True:
+        if stop_event.is_set():
+            print ("******************* Stopping detection thread.")
+            break
+        
         success, frame = cap.read()
+        # pour des raisons de perf ne traiter qu'une image sur 10
+        if not (cap.get(cv.CAP_PROP_POS_FRAMES) % 10 == 0):
+            continue
         if not success:
             break
-        else:
+        results = model(frame, conf=0.25, verbose=True)
+        
+        sleep(0.2)  # add a small delay to avoid high CPU usage
+        
 
-            # pour des raisons de perf ne traiter qu'une image sur 10
-            if (cap.get(cv.CAP_PROP_POS_FRAMES) % 10 == 0):
-                results = model(frame)
-                annotated_frame = results[0].plot()
+def gen_frames():
+    
+    global results
 
-                ret, buffer = cv.imencode('.jpg', annotated_frame)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    while True:
+        if results is None:
+            return
+
+        annotated_frame = results[0].plot()
+
+        ret, buffer = cv.imencode('.jpg', annotated_frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        sleep(0.2)  # add a small delay to avoid high CPU usage
 
 
 
@@ -48,8 +80,39 @@ def gen_frames():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/start_detection')
+def start_detection():
+    global DetectionThread, stop_event
+
+    if DetectionThread is not None and DetectionThread.is_alive():
+        response = {"message": "Detection is already running!"}
+        return jsonify(response)
+    
+    stop_event.clear()
+
+    DetectionThread = threading.Thread(target=detect_frames)
+    DetectionThread.daemon = True
+    DetectionThread.start()
+
+    response = {"message": "Detection started !"}
+    return jsonify(response)
+
+@app.route('/stop_detection')
+def stop_detection():
+    global DetectionThread, stop_event
+
+    if DetectionThread is None or not DetectionThread.is_alive():
+        response = {"message": "Detection is not running!"}
+        return jsonify(response)
+
+    stop_event.set()
+    DetectionThread.join()
+
+    response = {"message": "Detection stopped !"}
+    return jsonify(response)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(threaded=True, host='0.0.0.0')
 
 #release the video stream and close all windows
 cap.release()
